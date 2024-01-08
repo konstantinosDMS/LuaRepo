95b7c6d329e81bf31efff87a7d7803e18b7e8200
Konstantinos <konstantinosDMS@users.noreply.github.com>
2024-01-09 00:26:18 +0200

    Delete lua-programming_in_lua_4th_edition_notes/lua-programming_in_lua_4th_edition_notes directory

--- lua-programming_in_lua_4th_edition_notes/lua-programming_in_lua_4th_edition_notes/chapter_17/test_geometric_region_system.lua
+++
@@ -1,5 +0,0 @@
--- Exercise 17.2 --
-local grm = require('geometric_region_module')
-
-c1 = grm.disk(0, 0, 1)
-grm.plot(grm.difference(c1, grm.translate(c1, 0.3, 0)), 500, 500)
--- lua-programming_in_lua_4th_edition_notes/lua-programming_in_lua_4th_edition_notes/chapter_25/main_lua.lua
+++
@@ -1,7 +0,0 @@
-function testMe()
-    print('aaaa')
-end
-
-for i = 1, 10 do
-    testMe()
-end
