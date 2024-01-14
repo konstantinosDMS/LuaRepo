bbd2db904ef5743196532111df9ccaf6509e9473
Konstantinos <konstantinosDMS@users.noreply.github.com>
2024-01-14 16:55:12 +0200

    Update test_lua_exercise_25_8.lua

--- lua-programming_in_lua_4th_edition_notes/chapter_25/test_lua_exercise_25_8.lua
+++ lua-programming_in_lua_4th_edition_notes/chapter_25/test_lua_exercise_25_8.lua
@@ -1,1 +1,17 @@
+local debug = require "debug"
+local count = 0
 
+local function hook (event)
+    if event == "call" then
+        local info = debug.getinfo(1, "fn")
+    end
+    
+    count = count + 1
+    if count > 1000 then
+        error("script uses too much CPU")
+    end
+end
+
+for i = 1, 10 do
+    hook('call')    
+end
