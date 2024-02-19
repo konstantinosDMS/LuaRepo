0f13eab4973a855af342947a0c9eda4269422b53
Konstantinos <konstantinosDMS@users.noreply.github.com>
2024-02-19 15:52:17 +0200

    Update access_no_local_vars.lua

--- lua-programming_in_lua_4th_edition_notes/chapter_25/access_no_local_vars.lua
+++ lua-programming_in_lua_4th_edition_notes/chapter_25/access_no_local_vars.lua
@@ -4,30 +4,30 @@ function getvarvalue (name, level, isenv)
 	level = (level or 1) + 1
 	-- try local variables
 	for i = 1, math.huge do
-	local n, v = debug.getlocal(level, i)
-	if not n then break end
-	if n == name then
-		value = v
-		found = true
+		local n, v = debug.getlocal(level, i)
+		if not n then break end
+		if n == name then
+			value = v
+			found = true
+		end
 	end
-end
 
-if found then return "local", value end -- try non-local variables
-local func = debug.getinfo(level, "f").func -- upvalues from the _ENV environment
-for i = 1, math.huge do
-	local n, v = debug.getupvalue(func, i)
-	if not n then break end
-	if n == name then return "upvalue", v end
-end
+	if found then return "local", value end -- try non-local variables
+	local func = debug.getinfo(level, "f").func -- upvalues from the _ENV environment
+	for i = 1, math.huge do
+		local n, v = debug.getupvalue(func, i)
+		if not n then break end
+		if n == name then return "upvalue", v end
+	end
 
-if isenv then return "noenv" end -- if not upvalues, or local vars and isenv = true then return to avoid _ENV._ENV._ENV ... recursion
+	if isenv then return "noenv" end -- if not upvalues, or local vars and isenv = true then return to avoid _ENV._ENV._ENV ... recursion
 
--- avoid loop
--- not found; get value from the environment
-local _, env = getvarvalue("_ENV", level, true)
-if env then
-	return "global", env[name]
-else -- no _ENV available
-	return "noenv"
-end
+	-- avoid loop
+	-- not found; get value from the environment
+	local _, env = getvarvalue("_ENV", level, true)
+	if env then
+		return "global", env[name]
+	else -- no _ENV available
+		return "noenv"
+	end
 end
